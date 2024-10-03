545775a9bbfcd2f8759d976f20900ff112881d89
NaufalAbidAurizky <naufalabidau@gmail.com>
2024-10-03 17:40:20 +0700

    Pemilihan2

--- Pemilihan2Percobaan120.java
+++ Pemilihan2Percobaan120.java
@@ -13,10 +13,24 @@ public class Pemilihan2Percobaan120 {
         //     if ((tahun % 100) !=0) 
 
         //KEDUA
-        if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
-
+        // if ((tahun % 4 == 0 && tahun % 100 != 0) || (tahun % 400 == 0)) {
+        //      System.out.println("Tahun Kabisat");
+        // } else
+        //      System.out.println("Bukan Tahun Kabisat");
+        
+        //KETIGA
+        if (tahun % 4 == 0) { //apaka kelipatan 4
+            if (tahun % 100 == 0) { // kalau kelipatan 100
+                if (tahun % 400 == 0) { // kalau kelipatan 400
+                    System.out.print("Tahun Kabisat");
+                } else { //kelipatan 100 bukan 400
+                    System.out.print("Bukan Tahun Kabisat");
+                }
+            } else { //kelipatan 4 bukan 100
                 System.out.println("Tahun Kabisat");
-        } else
+            }
+        } else { //tidak kelipatan 4
             System.out.println("Bukan Tahun Kabisat");
+        }
     }
 }
